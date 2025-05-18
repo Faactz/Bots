cb21db97fd01d60ed3c00842eb57d0087cb65251
Faactz <factsbet@gmail.com>
2025-05-18 16:23:12 +0200

    Added port

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const client = new Client({
   ],
 });
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is running');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`HTTP server listening on port ${PORT}`);
+});
+
 client.once('ready', () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
 
